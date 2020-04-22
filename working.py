29d3e9088f74c93662e82f05d6fda959ea414f90
vyasn30 <vyasn30@outlook.com>
2020-04-23 01:29:03 +0530

    Tweaked working.py

--- working.py
+++ working.py
@@ -1,52 +1,59 @@
-def valid(board, pos, num):
-    for i in range(len(board[0])):
-        if board[pos[0]][i] == num and pos[1] != 1:
-            return False
-
-    for i in range(len(board[0])):
-        if board[i][pos[1]] == num and i != pos[0]:
-            return False
-
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
-
-    for i in range(box_y * 3, box_y * 3 + 3):
-        for j in range(box_x * 3, box_x * 3 + 3):
-            if board[i][j] == num:
-                return False
-
-    return True
+board = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
 
 
 def solve(bo):
-    find = findEmpty(bo) #tuple of empty position
-
+    find = find_empty(bo)
     if not find:
         return True
-
     else:
         row, col = find
 
-    for i in range(1, 10):
-        if valid(board, find, i):
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
             bo[row][col] = i
 
             if solve(bo):
                 return True
 
             bo[row][col] = 0
+
     return False
 
-def findEmpty(board):
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] == 0:
-                return (i, j)
 
-    return None
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
 
 
-def printBoard(bo):
+def print_board(bo):
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - - ")
@@ -57,25 +64,19 @@ def printBoard(bo):
 
             if j == 8:
                 print(bo[i][j])
-
             else:
                 print(str(bo[i][j]) + " ", end="")
 
 
-board = [
-    [6, 2, 0, 5, 3, 0, 0, 0, 0],
-    [5, 4, 0, 0, 9, 1, 0, 8, 0],
-    [0, 0, 7, 0, 0, 0, 0, 0, 2],
-    [0, 0, 1, 0, 0, 0, 0, 4, 3],
-    [0, 8, 0, 0, 0, 0, 5, 0, 0],
-    [4, 0, 0, 7, 6, 0, 0, 0, 0],
-    [2, 0, 0, 0, 0, 9, 0, 0, 4],
-    [0, 0, 0, 0, 2, 8, 0, 6, 0],
-    [0, 0, 9, 0, 0, 3, 0, 7, 1]
-]
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
 
-printBoard(board)
+    return None
+
+print_board(board)
 solve(board)
-
-print("solved")
-printBoard(board)
+print("___________________")
+print_board(board)
